55c999b8699abe573de5e3968cbf2af870afd4df
vladimir borovic <vladimir.borovic@ellation.com>
2021-09-27 18:44:28 +0300

    Write json lines to s3

--- S3Sink/src/main/java/com/amazonaws/services/kinesisanalytics/S3StreamingSinkJob.java
+++ S3Sink/src/main/java/com/amazonaws/services/kinesisanalytics/S3StreamingSinkJob.java
@@ -17,15 +17,14 @@ import java.util.Properties;
 
 public class S3StreamingSinkJob {
     private static final String region = "us-west-2";
-    private static final String inputStreamName = "ExampleInputStream";
-    private static final String s3SinkPath = "s3a://ka-app-<username>/data";
+    private static final String inputStreamName = "alpha0-data-event-validator";
+    private static final String s3SinkPath = "s3a://codemobs-datalab/hudi/new/";
 
     private static DataStream<String> createSourceFromStaticConfig(StreamExecutionEnvironment env) {
 
         Properties inputProperties = new Properties();
         inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, region);
-        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION,
-                "LATEST");
+        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
         return env.addSource(new FlinkKinesisConsumer<>(inputStreamName,
                 new SimpleStringSchema(),
                 inputProperties));
@@ -45,27 +44,40 @@ public class S3StreamingSinkJob {
 
         DataStream<String> input = createSourceFromStaticConfig(env);
 
-        input.flatMap(new Tokenizer()) // Tokenizer for generating words
-                .keyBy(0) // Logically partition the stream for each word
-                .timeWindow(Time.minutes(1)) // Tumbling window definition
-                .sum(1) // Sum the number of words per partition
-                .map(value -> value.f0 + " count: " + value.f1.toString() + "\n")
-                .addSink(createS3SinkFromStaticConfig());
+//        input.flatMap(new Tokenizer()) // Tokenizer for generating words
+//                .keyBy(0) // Logically partition the stream for each word
+//                .timeWindow(Time.minutes(1)) // Tumbling window definition
+//                .sum(1) // Sum the number of words per partition
+//                .map(value -> value.f0 + " count: " + value.f1.toString() + "\n")
+//                .map(value -> value)
+//                .addSink(createS3SinkFromStaticConfig());
+
+//        input.flatMap(new Tokenizer()) // Tokenizer for generating words
+//                .map(value -> value)
+//                .addSink(createS3SinkFromStaticConfig());
+        input.addSink(createS3SinkFromStaticConfig());
 
         env.execute("Flink S3 Streaming Sink Job");
     }
 
-    public static final class Tokenizer
-            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+    public static final class Tokenizer implements FlatMapFunction<String, String> {
 
         @Override
-        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-            String[] tokens = value.toLowerCase().split("\\W+");
-            for (String token : tokens) {
-                if (token.length() > 0) {
-                    out.collect(new Tuple2<>(token, 1));
-                }
-            }
+        public void flatMap(String value, Collector<String> out) {
+            out.collect(value);
         }
     }
+//    public static final class Tokenizer
+//            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+//
+//        @Override
+//        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+//            String[] tokens = value.toLowerCase().split("\\W+");
+//            for (String token : tokens) {
+//                if (token.length() > 0) {
+//                    out.collect(new Tuple2<>(token, 1));
+//                }
+//            }
+//        }
+//    }
 }
